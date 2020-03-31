b80b159f59c9766553c520585cc29c1a8fc6c109
Alexandru Calugari <afishr00@gmail.com>
2020-04-01 02:49:19 +0300

    Update REPL (#4)
    * Add basic REPL
    
    * Update REPL due to adding parser

--- repl/repl.go
+++ repl/repl.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"io"
 	"rainbow/lexer"
-	"rainbow/token"
+	"rainbow/parser"
 )
 
 const PROMPT = ">> "
@@ -23,9 +23,23 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("Type \"%s\", Literal \"%s\"\n", tok.Type, tok.Literal)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, "Whoops! Some erros occured\n")
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
